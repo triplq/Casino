ee161dabb1407558bc8a373e8f625e4697a76fa1
Alexey <138684222+triplq@users.noreply.github.com>
2025-11-06 22:27:12 +0300

    Delete build directory

--- build/unknown_f8dfd7-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Casino_FILE /Users/alexey/Casino/build/unknown_f8dfd7-Debug/Casino.app/Contents/MacOS/Casino)
-set(__QT_DEPLOY_TARGET_Casino_TYPE EXECUTABLE)
--- build/unknown_f8dfd7-Debug/CMakeFiles/Casino.dir/qrc_casino_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Casino.dir/qrc_casino_resources.cpp.o: \
-  /Users/alexey/Casino/build/unknown_f8dfd7-Debug/qrc_casino_resources.cpp
